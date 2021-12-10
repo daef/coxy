01df7ac14dc7aef5d31a59f9ed8489070fcbdb72
daef <daef@sourcemonk.com>
2021-12-11 00:43:21 +0100

    basics seem to work

--- web/script.js
+++ web/script.js
@@ -1,2 +1,29 @@
-alert('hello world')
+
+function get_ws_uri() {
+  var loc = window.location, uri
+  if (loc.protocol === "https:") {
+    uri = "wss:"
+  } else {
+    uri = "ws:"
+  }
+  uri += "//" + loc.host
+  uri += loc.pathname + "ws"
+  return uri
+}
+
+function main() {
+  const socket = new WebSocket(get_ws_uri())
+  socket.addEventListener('open', function (event) {
+    socket.send(JSON.stringify({'type': 'subscribe', 'filter': 'stdout|stderr|exit|hello'}))
+  })
+  socket.addEventListener('message', function (event) {
+    msg = JSON.parse(event.data)
+    console.log(msg)
+  })
+  document.getElementById('reset').addEventListener('click', function(event) {
+    socket.send(JSON.stringify({'type': 'reset'}))
+  })
+}
+
+main()
 
